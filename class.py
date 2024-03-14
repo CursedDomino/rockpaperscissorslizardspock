1bb0354977aff6ddf1599529d82a71d60c9b695f
CursedDomino <dillon.daughrity@outlook.com>
2024-03-14 11:27:23 -0500

    Starting Code

--- class.py
+++ class.py
@@ -1,1 +1,13 @@
-print('Hello World')
+class Symbol():
+    def __init__(self):
+        self.defeats = []
+        self.defeated_by = []
+        self.draws = []
+
+    def determine_winner(self, cpu_symbol):
+        if cpu_symbol in self.defeats:
+            return 'Win'
+        elif cpu_symbol in self.defeated_by:
+            return 'Lose'
+        else:
+            return 'Draw'
